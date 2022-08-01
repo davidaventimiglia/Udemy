dcb6b868f9065bb764a958c222ff1a590c1e20e8
David A. Ventimiglia <davidaventimiglia@neptunestation.com>
2022-08-01 06:59:09 -0700

    Added the snowflakes

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,18 +1,30 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
+const seasonConfig = {
+	summer: {
+		text: "Let's hit the beach!",
+		iconName: "sun"
+	},
+	winter: {
+		text: "Burr, it's cold!",
+		iconName: "snowflake"
+	}
+};
+
 const getSeason = (lat, month) => {
-	if (month > 2 && month < 9) {
-		return lat > 0 ? 'summer' : 'winter';
-	}
-	else {
-		return lat > 0 ? 'winter' : 'summer';
-	}
+	return (month > 2 && month < 9) ?
+		lat > 0 ? 'summer' : 'winter':
+		lat > 0 ? 'winter' : 'summer';
 }
 
 const SeasonDisplay = (props) => {
 	const season = getSeason(props.lat, new Date().getMonth());
-	return <div>
-					 Latitude: {props.lat}
+	const {text, iconName} = seasonConfig[season];
+	return <div className={`season-display ${season}`}>
+					 <i className={`icon-left massive ${iconName} icon`}/>
+					 <h1>{text}</h1>
+					 <i className={`icon-right massive ${iconName} icon`}/>
 				 </div>;	
 };
 
