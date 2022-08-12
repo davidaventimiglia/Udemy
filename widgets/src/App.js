03c9836db6d8a7afbfbb9518f0531308c8bea1b9
David A. Ventimiglia <davidaventimiglia@neptunestation.com>
2022-08-12 05:03:15 -0700

    Adding widgets

---
+++ widgets/src/App.js
@@ -0,0 +1,41 @@
+import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
+import React, { useState, useEffect } from 'react';
+import Search from './components/Search';
+import Translate from './components/Translate';
+
+const items = [
+	{
+		title: "What is React?",
+		content: "React is a front-end Javascript framework."
+	},
+	{
+		title: "Why use React?",
+		content: "React is a favorite JS library among engineers."
+	},
+	{
+		title: "How do you use React?",
+		content: "You use React by creating components."
+	}
+];
+
+const options = [
+	{
+		label: 'The Color Red',
+		value: 'red'
+	},
+	{
+		label: 'The Color Green',
+		value: 'green'
+	},
+	{
+		label: 'The Color Blue',
+		value: 'blue'
+	},
+];
+
+export default () => {
+  return <div>
+           <Translate/>
+         </div>;
+};
