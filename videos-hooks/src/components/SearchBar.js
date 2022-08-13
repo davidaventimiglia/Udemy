a07cc7b119230c32f484cc000c68243ee968cfd1
David A. Ventimiglia <davidaventimiglia@neptunestation.com>
2022-08-13 09:23:28 -0700

    Fixed the SearchBar title

--- videos-hooks/src/components/SearchBar.js
+++ videos-hooks/src/components/SearchBar.js
@@ -13,7 +13,7 @@ const SearchBar = ({onFormSubmit}) => {
 						 onSubmit={onSubmit}
 						 className="ui form">
 						 <div className="field">
-							 <label>Search For A Video</label>
+							 <label>Video Search</label>
 							 <input
 								 type="text"
 								 onChange={e => setTerm(e.target.value)}
